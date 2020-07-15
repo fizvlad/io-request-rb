e13323420f51c0f8bd9f7a5461c57fc5009522d4
Fizvlad <fizvlad@mail.ru>
2020-07-16 00:20:58 +0300

    Remove old files

--- test/io_request_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'test_helper'
-
-class IORequestTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::IORequest::VERSION
-  end
-end
