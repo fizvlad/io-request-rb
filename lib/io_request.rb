c36aad153766516fa9f6d4b89ed377dd073ad669
Fizvlad <fizvlad@mail.ru>
2020-07-16 00:21:44 +0300

    Do not require old files

--- lib/io_request.rb
+++ lib/io_request.rb
@@ -3,8 +3,7 @@
 require_relative 'io_request/version'
 require_relative 'io_request/utility/with_prog_name'
 require_relative 'io_request/utility/multi_thread'
-require_relative 'io_request/logging'
-require_relative 'io_request/message'
+
 require_relative 'io_request/client'
 
 # Main module.
