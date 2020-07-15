f2d28fbf65edb7b60c88b31120a9c0d044d4f4ad
Fizvlad <fizvlad@mail.ru>
2020-07-16 00:51:47 +0300

    Form test

--- test/client_io_test.rb
+++ test/client_io_test.rb
@@ -5,45 +5,42 @@ require_relative 'test_helper'
 class ClientIOTest < Minitest::Test
   def setup
     @r1, @w1 = IO.pipe
-    @r2, @w2 = IO.pipe
+    @client1 = IORequest::Client.new
+    @client1.open read: @r1, write: @w2
 
-    @client_1 = IORequest::Client.new read: @r1, write: @w2
-    @client_2 = IORequest::Client.new read: @r2, write: @w1
+    @r2, @w2 = IO.pipe
+    @client2 = IORequest::Client.new
+    @client2.open read: @r2, write: @w1
   end
 
   def teardown
-    @r1.close
-    @w1.close
-    @r2.close
-    @w2.close
+    @client1.close
+    @client2.close
   end
 
   def test_simple_request
-    @client_2.respond do |_request|
+    @client2.respond do |_data|
       { num: 1, string: 'str' }
     end
 
-    @client_1.request sync: true do |response|
-      assert_equal(1, response.data[:num])
-      assert_equal('str', response.data[:string])
-      assert_nil(response.data[:sync])
-      assert_nil(response.data[:timeout])
+    @client1.request do |data|
+      assert_equal(1, data[:num])
+      assert_equal('str', data[:string])
+      assert_nil(data[:sync])
+      assert_nil(data[:timeout])
     end
   end
 
   def test_intersecting_requests
     total = 10
 
-    @client_2.respond do |request|
-      sleep_time = request.data[:sleep_time]
-      sleep sleep_time
-      { num: request.data[:num] }
+    @client2.respond do |data|
+      sleep data[:sleep_time]
+      { num: data[:num] }
     end
 
     requests = Array.new(total) do |i|
-      @client_1.request data: { sleep_time: total - i, num: i } do |response|
-        assert_equal(i, response.data[:num])
-      end
+      @client1.request({ sleep_time: total - i, num: i }) { |data| assert_equal(i, data[:num]) }
     end
 
     requests.each(&:join)
