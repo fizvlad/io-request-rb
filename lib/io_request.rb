2551e145e734964b84c3902278cd118c10c115ae
Fizvlad <fizvlad@mail.ru>
2020-07-17 01:05:29 +0300

    Add formatter to logger

--- lib/io_request.rb
+++ lib/io_request.rb
@@ -14,11 +14,16 @@ require 'logger'
 module IORequest
   # @return [Logger]
   def self.logger
-    @@logger ||= Logger.new(STDOUT) # rubocop:disable Style/ClassVars
+    @@logger ||= Logger.new( # rubocop:disable Style/ClassVars
+      STDOUT,
+      formatter: proc do |severity, datetime, progname, msg|
+        "[#{datetime}] #{severity} - #{progname}:\t #{msg}\n"
+      end
+    )
   end
 
   # @param new_logger [Logger]
   def self.logger=(new_logger)
-    @@logger =new_logger # rubocop:disable Style/ClassVars
+    @@logger = new_logger # rubocop:disable Style/ClassVars
   end
 end
