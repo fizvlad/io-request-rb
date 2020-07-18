f7d2fbc20cf60a8df9534f5d3df144fc51efec57
Fizvlad <fizvlad@mail.ru>
2020-07-18 09:41:10 +0300

    Add WithID mixin

--- lib/io_request.rb
+++ lib/io_request.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'io_request/version'
-require_relative 'io_request/utility/with_prog_name'
 require_relative 'io_request/utility/multi_thread'
+require_relative 'io_request/utility/with_id'
+require_relative 'io_request/utility/with_prog_name'
 
 require_relative 'io_request/authorizer'
 require_relative 'io_request/message'
