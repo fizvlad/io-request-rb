539019c522155f78ee46acef3ec2513adc985870
Fizvlad <fizvlad@mail.ru>
2020-07-18 13:31:54 +0300

    Move logging methods into separate file

--- lib/io_request.rb
+++ lib/io_request.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+# Main module.
+module IORequest; end
+
 require_relative 'io_request/version'
+require_relative 'io_request/logging'
 require_relative 'io_request/utility/multi_thread'
 require_relative 'io_request/utility/with_id'
 require_relative 'io_request/utility/with_prog_name'
@@ -8,23 +12,3 @@ require_relative 'io_request/utility/with_prog_name'
 require_relative 'io_request/authorizer'
 require_relative 'io_request/message'
 require_relative 'io_request/client'
-
-require 'logger'
-
-# Main module.
-module IORequest
-  # @return [Logger]
-  def self.logger
-    @@logger ||= Logger.new( # rubocop:disable Style/ClassVars
-      STDOUT,
-      formatter: proc do |severity, datetime, progname, msg|
-        "[#{datetime}] #{severity} - #{progname}:\t #{msg}\n"
-      end
-    )
-  end
-
-  # @param new_logger [Logger]
-  def self.logger=(new_logger)
-    @@logger = new_logger # rubocop:disable Style/ClassVars
-  end
-end
